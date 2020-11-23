a674e6e41f0a7035c1820a380c436e94067d7488
Eshan Chatterjee <eshan2001221@gmail.com>
2020-11-24 00:27:31 +0530

    Vertex Array Class Update

--- src/Abstracion/VertexArray.h
+++ src/Abstracion/VertexArray.h
@@ -1,14 +1,15 @@
 #pragma once
-
 #include "VertexBuffer.h"
-#include "VertexBufferLayout.h"
+
+class VertexBufferLayout;
 
 typedef unsigned int uint;
 
+/* Class Vertex Array */
 class VertexArray
 {
 private:
-	uint m_RendererID;
+	uint m_RendererID;   //? ID for a specific object
 
 public:
 	VertexArray();
