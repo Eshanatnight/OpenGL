3dab0ca46d8c2b85740e849d4cbe6232559ca339
Eshan Chatterjee <eshan2001221@gmail.com>
2020-11-24 00:28:46 +0530

    Renderer Class Added

--- src/Abstracion/Renderer.cpp
+++ src/Abstracion/Renderer.cpp
@@ -1,8 +1,25 @@
 #include "Renderer.h"
+#include <iostream>
 
 void GLClearError()
 {
 	while (glGetError() != GL_NO_ERROR);
+}
+
+void setGLVersionCORE(const int& major, const int& minor)
+{
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+
+
+void setGLVersionCOMPAT(const int& major, const int& minor)
+{
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 }
 
 bool GLLogCall(const char* function, const char* file, int line)
@@ -14,4 +31,29 @@ bool GLLogCall(const char* function, const char* file, int line)
 		return false;
 	}
 	return true;
+}
+
+void setFrameRate(const uint& frmRate)
+{
+	/*
+	! Important
+	Sets the frame rate via LValue references
+	uses a const uint reference instead of an LValue Reference
+	so in the event the frame rate needs to be set by a RValue
+	without the use of the move assignment operator
+	*/
+	glfwSwapInterval(frmRate);
+}
+
+void Renderer::Clear() const
+{
+	GlCall(glClear(GL_COLOR_BUFFER_BIT));
+}
+
+void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const
+{
+	shader.Bind();
+	va.Bind();
+	ib.Bind();
+	GlCall(glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr));
 }
--- src/Abstracion/Renderer.h
+++ src/Abstracion/Renderer.h
@@ -1,7 +1,9 @@
 #pragma once
-
 #include <GL\glew.h>
-#include <iostream>
+#include <GLFW/glfw3.h>
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "Shader.h"
 
 #define ASSERT(x) if (!(x)) __debugbreak();
 
@@ -9,8 +11,25 @@
 	x;\
 	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 
-typedef unsigned int uint;
+typedef unsigned int uint;    // ! for ease of use
 
+/* Clears GL Error List */
 void GLClearError();
 
+/* For Setting up the OpenGl Version and Profile */
+void setGLVersionCORE(const int& major, const int& minor);
+void setGLVersionCOMPAT(const int& major, const int& minor);
+
+/* Logs Errors to Console */
 bool GLLogCall(const char* function, const char* file, int line);
+
+/* Sets Render Framerate */
+void setFrameRate(const uint& frmRate);
+
+/* Renderer Class */
+class Renderer
+{
+public:
+	void Clear() const;
+	void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+};
